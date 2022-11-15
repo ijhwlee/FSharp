b9c822a3c2c9bc0bd8574984d62024edf7248a53
ijhwlee <hwlee@inje.ac.kr>
2022-11-16 05:07:49 +0900

    first program

---
+++ Chap01/HelloFS/HelloFS/Program.fs
@@ -0,0 +1,12 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+let square x = x * x          // Note that no parens are used.
+let result = square 3                      // Now run the function. Again, no parens.
+printfn "3^2 = %i" result
+// define the sumOfSquares function
+let sumOfSquares n =
+   [1..n] |> List.map square |> List.sum
+
+// try it
+let sum2 = sumOfSquares 100
+printfn "sum of squares upto 100 = %i" sum2
