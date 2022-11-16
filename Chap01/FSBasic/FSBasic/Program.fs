dcfaff13d25a32ebae6b16a3a0a2b259d94677df
ijhwlee <hwlee@inje.ac.kr>
2022-11-16 20:14:12 +0900

    second program

---
+++ Chap01/FSBasic/FSBasic/Program.fs
@@ -0,0 +1,13 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+let myOne = 1
+let myTwo = 2
+let hello = "Hello"
+let letterA = 'a'
+let mutable isEnabled = true
+isEnabled <- false
+printfn $"isEnabled {isEnabled}"
+
+let add x y = x+y
+printfn $"add one two = {add myOne myTwo}"
+
